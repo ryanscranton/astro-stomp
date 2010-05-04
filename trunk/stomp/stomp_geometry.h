67558843a6fe333a3e66469023880bfc9c97919f
ryan.scranton <ryan.scranton@303128e0-2175-11df-99f8-b513f4f1e6c6>
2010-05-04 15:46:08 +0000

    Added LongitudeBound, LatitudeBound and LatLonBound classes to stomp_geometry.  Still need to make appropriate unit tests for these classes.

--- trunk/stomp/stomp_geometry.h
+++ trunk/stomp/stomp_geometry.h
@@ -8,10 +8,19 @@
 // a way as to make the analysis of that data as algorithmically efficient as
 // possible.
 //
-//  Stuff about footprints
+// While the general principle of the library is to describe regions on the
+// sphere as collections of pixels, it can occasionally be useful to have a
+// simple geometric description of a region as well.  The GeometricBound class
+// and its derivatives fills this role.  Each GeometricBound instance must be
+// able to return its bounded area, its angular bounds (in survey coordinates)
+// and indicate whether an input point is inside or outside of its allowed
+// area.  For more complicated geometric tasks (like finding the intersection
+// of two bounds), the proper procedure would be to create Maps from the
+// GeometricBound objects (with the appropriate constructor) and perform those
+// operations on their pixelized counterparts.
 
-#ifndef STOMP_FOOTPRINT_H
-#define STOMP_FOOTPRINT_H
+#ifndef STOMP_GEOMETRY_H
+#define STOMP_GEOMETRY_H
 
 #include <stdint.h>
 #include <math.h>
@@ -25,7 +34,9 @@ class GeometricBound;
 class CircleBound;
 class WedgeBound;
 class PolygonBound;
-class Footprint;
+class LongitudeBound;
+class LatitudeBound;
+class LatLonBound;
 
 typedef std::vector<CircleBound> CircleVector;
 typedef CircleVector::iterator CircleIterator;
@@ -36,8 +47,14 @@ typedef WedgeVector::iterator WedgeIterator;
 typedef std::vector<PolygonBound> PolygonVector;
 typedef PolygonVector::iterator PolygonIterator;
 
-typedef std::vector<Footprint> FootprintVector;
-typedef FootprintVector::iterator FootprintIterator;
+typedef std::vector<LongitudeBound> LongitudeVector;
+typedef LongitudeVector::iterator LongitudeIterator;
+
+typedef std::vector<LatitudeBound> LatitudeVector;
+typedef LatitudeVector::iterator LatitudeIterator;
+
+typedef std::vector<LatLonBound> LatLonVector;
+typedef LatLonVector::iterator LatLonIterator;
 
 class GeometricBound {
   // The starting point for a footprint is some analytic geometric bound.  This
@@ -136,6 +153,61 @@ class PolygonBound : public GeometricBound {
   AngularVector ang_;
   std::vector<double> x_, y_, z_, dot_;
   uint32_t n_vert_;
+};
+
+class LongitudeBound : public GeometricBound {
+  // Simpler than the PolygonBound class, this bound simply includes all of the
+  // area on the sphere between a given set of bounds in longitude, for a
+  // particular set of spherical coordinates.  Input values of longitude should
+  // be in degrees.
+
+ public:
+  LongitudeBound(double min_longitude, double max_longitude,
+		 AngularCoordinate::Sphere sphere);
+  virtual ~LongitudeBound();
+  virtual bool CheckPoint(AngularCoordinate& ang);
+  virtual bool FindAngularBounds();
+  virtual bool FindArea();
+
+ private:
+  double min_longitude_, max_longitude_;
+  bool continuous_longitude_;
+  AngularCoordinate::Sphere sphere_;
+};
+
+class LatitudeBound : public GeometricBound {
+  // Similar to the LongitudeBound class, but for a given range in latitude.
+
+ public:
+  LatitudeBound(double min_latitude, double max_latitude,
+		 AngularCoordinate::Sphere sphere);
+  virtual ~LatitudeBound();
+  virtual bool CheckPoint(AngularCoordinate& ang);
+  virtual bool FindAngularBounds();
+  virtual bool FindArea();
+
+ private:
+  double min_latitude_, max_latitude_;
+  AngularCoordinate::Sphere sphere_;
+};
+
+class LatLonBound : public GeometricBound {
+  // Similar to the LongitudeBound class, but for a given range in both
+  // longitude and latitude.
+
+ public:
+  LatLonBound(double min_latitude, double max_latitude,
+	      double min_longitude, double max_longitude,
+	      AngularCoordinate::Sphere sphere);
+  virtual ~LatLonBound();
+  virtual bool CheckPoint(AngularCoordinate& ang);
+  virtual bool FindAngularBounds();
+  virtual bool FindArea();
+
+ private:
+  double min_latitude_, max_latitude_, min_longitude_, max_longitude_;
+  bool continuous_longitude_;
+  AngularCoordinate::Sphere sphere_;
 };
 
 } // end namespace Stomp
