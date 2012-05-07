83fbe9cd9ffcd80b9bcfd47ced794381d8296994
morrison.chrisb@gmail.com <morrison.chrisb@gmail.com@303128e0-2175-11df-99f8-b513f4f1e6c6>
2012-05-07 00:54:51 +0000

    New Classes in correlation.py and kernely.py named GalaxyGalaxyLensing. As stated in the names, they caculate the the galaxy-galaxy lensing signal given lens and source distributions and a cosmology.
    Currently their functional forms and normalizations are guesstimated. If i've done something wrong let me know.

--- trunk/experimental/theory/correlation.py
+++ trunk/experimental/theory/correlation.py
@@ -124,11 +124,12 @@ Defaults to linear_power
 
 class MagCorrelation(Correlation):
     def __init__(self, theta_min, theta_max, 
-                 window_function_a, window_function_b, 
+                 window_function_galaxy, window_function_convergence, 
                  cosmo_dict=None, input_hod=None, halo_dict=None, 
                  powSpec='power_gm', **kws):
         Correlation.__init__(self, theta_min, theta_max,
-                             window_function_a, window_function_b,
+                             window_function_galaxy,
+                             window_function_convergence,
                              cosmo_dict, input_hod, halo_dict, powSpec,
                              **kws)
 
@@ -143,12 +144,12 @@ class MagCorrelation(Correlation):
 class AutoCorrelation(Correlation):
 
     def __init__(self, theta_min, theta_max, 
-                 window_function, 
+                 window_function_galaxy, 
                  cosmo_dict=None, input_hod=None, halo_dict=None, 
                  powSpec='power_gg', **kws):
-        self.window_function = window_function
+        self.window_function = window_function_galaxy
         Correlation.__init__(self, theta_min, theta_max,
-                             window_function, window_function,
+                             window_function_galaxy, window_function_galaxy,
                              cosmo_dict, input_hod, halo_dict, powSpec,
                              **kws)
 
@@ -160,34 +161,42 @@ class AutoCorrelation(Correlation):
             dk*k*self.power_spec(k)/(self.D_z*self.D_z)*
             self.kernel.kernel(numpy.log(k*theta)))
 
-class AutoCorrelationDeltaFunction(Correlation):
+class GalaxyGalaxyLensing(Correlation):
 
-    def __init__(self, theta_min, theta_max, redshift, input_cosmology=None,
-               input_hod=None, input_halo=None, powSpec=None, **kws):
+    def __init__(self, theta_min, theta_max, 
+                 window_function_galaxy, window_function_convergence, 
+                 cosmo_dict=None, input_hod=None, halo_dict=None, 
+                 powSpec='power_gm', **kws):
         self.log_theta_min = numpy.log10(theta_min)
         self.log_theta_max = numpy.log10(theta_max)
-        self.theta_array = numpy.logspace(self.log_theta_min, 
-                                       self.log_theta_max, 20)
+        self.theta_array = numpy.logspace(self.log_theta_min,
+                                      self.log_theta_max, 50)
+        if theta_min==theta_max:
+            self.log_theta_min = numpy.log10(theta_min)
+            self.log_theta_max = numpy.log10(theta_min)
+            self.theta_array = numpy.array([theta_min])
         self.wtheta_array= numpy.zeros(self.theta_array.size)
-        self.z = redshift
+
+        # Hard coded, but we shouldn't expect halos outside of this range.
         self._k_min = 0.001
         self._k_max = 100.0
-        
-        self.ln_ktheta_min = numpy.log(self._k_min*theta_min)
-        self.ln_ktheta_max = numpy.log(self._k_max*theta_max)
 
-        dln_ktheta = (self.ln_ktheta_max - self.ln_ktheta_min)/200.0
-        self._ln_ktheta_array = numpy.arange(
-            self.ln_ktheta_min, self.ln_ktheta_max + dln_ktheta, dln_ktheta)
+        if cosmo_dict == None:
+            cosmo_dict = defaults.default_cosmo_dict
+        self.cosmo_dict = cosmo_dict
 
-        if input_cosmology == None:
-            self.cosmology = cosmology.SingleEpoch(redshift,
-                                                   param.CambParams(**kws))
-        else:
-            self.cosmology = input_cosmology
-        self.halo = input_halo
-            
+        self.kernel = kernel.GalaxyGalaxyKernel(
+            self._k_min*theta_min, 
+            self._k_max*theta_max,
+            window_function_galaxy, window_function_convergence,
+            cosmo_dict)
 
+        self.D_z = self.kernel.cosmo.growth_factor(self.kernel.z_bar)
+                      
+        if halo_dict is None:
+            halo_dict = defaults.default_halo_dict
+        self.halo = halo.HaloExclusion(input_hod, self.kernel.z_bar, 
+                                       cosmo_dict, halo_dict)
         if powSpec==None:
             powSpec = 'linear_power'
         try:
@@ -196,99 +205,11 @@ class AutoCorrelationDeltaFunction(Correlation):
             print "WARNING: Invalid input for power spectra varriable,"
             print "\t setting to linear_power"
             self.power_spec = self.halo.__getattribute__('linear_power')
-        
+
     def _correlation_integrand(self, ln_k, theta):
         dln_k = 1.0
         k = numpy.exp(ln_k)
         dk = k*dln_k
-        return (dk*k*self.power_spec(k)*
-                special.j0(k*theta*cosmo.comoving_distance()))
-
-class CorrelationDeltaFunction(Correlation):
-    
-    """
-    Correlation function for delta functions assumed for the redshift 
-    distribution of both populations being correlated.
-    """
-
-    def __init__(self, theta_min, theta_max, redshift_a, redshift_b, 
-                 cosmo_dict=None, input_hod=None, halo_dict=None, 
-                 powSpec=None, **kws):
-        self.log_theta_min = numpy.log10(theta_min)
-        self.log_theta_max = numpy.log10(theta_max)
-        self.theta_array = numpy.logspace(self.log_theta_min, 
-                                       self.log_theta_max, 20)
-        if theta_min == theta_max:
-            self.log_theta_min = numpy.log10(theta_min)
-            self.log_theta_max = numpy.log10(theta_min)
-            self.theta_array = numpy.array([theta_min])
-        self.wtheta_array= numpy.zeros(self.theta_array.size)
-
-        self.z_a = redshift_a
-        self.z_b = redshift_b
-
-        # Hard coded, but we shouldn't expect halos outside of this range.
-        self._k_min = 0.001
-        self._k_max = 100.0
-        
-        self.ln_ktheta_min = numpy.log(self._k_min*theta_min)
-        self.ln_ktheta_max = numpy.log(self._k_max*theta_max)
-        dln_ktheta = (self.ln_ktheta_max - self.ln_ktheta_min)/200.0
-        self._ln_ktheta_array = numpy.arange(
-            self.ln_ktheta_min, self.ln_ktheta_max + dln_ktheta, dln_ktheta)
-
-        if cosmo_dict == None:
-            cosmo_dict = param.CambParams(**kws)
-        self.cosmology = cosmology.MultiEpoch(0,self.z_b+self.z_a,cosmo_dict)
-        self.xiA_a = self.cosmology.comoving_distance(self.z_a)
-        self.xiA_b = self.cosmology.comoving_distance(self.z_b)
-        print (self.xiA_a, self.xiA_b, self.cosmology.h, 
-               self.cosmology.omega_m0, 1+self.z_a, self.xiA_b-self.xiA_a,
-               self.xiA_b*self.xiA_a,
-               self.cosmology.growth_factor(self.z_a))
-        self.cosmology_prefactor = (3.0/2.0*(self.cosmology.h*100/
-                                             speed_of_light)**2*
-                                    self.cosmology.omega_m0*
-                                    (1+self.z_a)*
-                                    (self.xiA_b-self.xiA_a)/
-                                    (self.xiA_b*self.xiA_a))
-        print self.cosmology_prefactor
-
-        self.ln_s_min = numpy.log(self._k_min*self.xiA_a)
-        self.ln_s_max = numpy.log(self._k_max*self.xiA_b)
-        dln_s = (self.ln_s_max - self.ln_s_min)/200.0
-        self._ln_s_array = numpy.arange(
-            self.ln_s_min, self.ln_s_max + dln_s, dln_s)
-                      
-        if halo_dict is None:
-            halo_dict = param.HaloModelParams(**kws)
-        self.halo = halo.Halo(input_hod, self.z_a, cosmo_dict, halo_dict)
-
-    def compute_correlation(self):
-        for idx in xrange(self.theta_array.size):
-            print "Computing Theta at", self.theta_array[idx]
-            wtheta, wtheta_err = integrate.quad(self._correlation_integrand, 
-                                                self.ln_s_min,
-                                                self.ln_s_max,
-                                                args=(self.theta_array[idx]),
-                                                limit=200)
-            self.wtheta_array[idx] = self.cosmology_prefactor*wtheta
-
-    def _correlation_integrand(self, ln_s, theta):
-        dln_s = 1.0
-        s = numpy.exp(ln_s)
-        ds = s*dln_s
-        k = s/self.xiA_a
-        return (ds*s*self.halo.power_mm(k)
-                *special.j0(s*theta))
-
-    def write_kernel(self, output_file_name):
-        f = open(output_file_name, "w")
-        theta = 1.0/60.0*numpy.pi/180.0
-        for ln_s in self._ln_s_array:
-            s = numpy.exp(ln_s)
-            k = s/self.xiA_a
-            f.write("%1.10f %1.10f %1.10f\n" % (k, self._correlation_integrand(
-                        ln_s, theta)/s**2,special.j0(s*theta)))
-        f.close()
-
+        return 1.0/(4.0*numpy.pi)*(
+            dk*k*self.power_spec(k)/(self.D_z*self.D_z)*
+            self.kernel.kernel(numpy.log(k*theta)))
